067c86109db98109dc6cfc631ef09a166e1c7fdd
Jeffrey M Josanne <jeffjosesvlj@gmail.com>
2017-06-23 10:38:53 +0530

    Scene count added

--- search.py
+++ search.py
@@ -7,6 +7,7 @@ end_times=[]
 actual_start_time=[]
 actual_end_time=[]
 with codecs.open('test.srt', 'r', 'utf8') as file:
+    sceneCount=0
     csvWriter = csv.writer(open('test.csv', 'w'))
     lines = file.readlines()
     for line in lines:
@@ -19,14 +20,16 @@ with codecs.open('test.srt', 'r', 'utf8') as file:
         end_at = 0
         one_start_time_list=one_temp_start_time.split(':')
         minutes = int(one_start_time_list[1])
-        seconds =int(one_start_time_list[2].replace(',', ''))
+        secondsTrimmer = one_start_time_list[2].split(',')[0]
+        seconds =int(secondsTrimmer.replace(',', ''))
         if(int(one_start_time_list[0])!=0):
             minutes = minutes + int(one_start_time_list[0])*60
         start_at =seconds +minutes * 60
         actual_start_time.append(start_at)
         one_end_time_list = one_temp_end_time.split(':')
         minutes = int(one_end_time_list[1])
-        seconds = int(one_end_time_list[2].replace(',', ''))
+        secondsTrimmer = one_end_time_list[2].split(',')[0]
+        seconds = int(secondsTrimmer.replace(',', ''))
         if (int(one_end_time_list[0]) != 0):
             minutes = minutes + int(one_end_time_list[0]) * 60
         end_at = seconds + minutes *60
@@ -36,9 +39,10 @@ if(len(actual_start_time)>0):
 
 for one_actual_start_time, one_actual_end_time in zip(actual_start_time, actual_end_time):
     diff = (one_actual_start_time) - (one_actual_end_time)
-    if((diff)>0):
-        csvWriter.writerow([one_actual_start_time, one_actual_end_time])
-        print(diff/1000)
-        print(str(one_actual_start_time/1000)+'-->'+str(one_actual_end_time/1000))
+    if((diff)>5):
+        csvWriter.writerow([one_actual_start_time, one_actual_end_time, sceneCount])
+        sceneCount += 1
+        print(diff)
+        print(str(timedelta(seconds=one_actual_start_time))+'-->'+str(timedelta(seconds=one_actual_end_time)))
 
     # subprocess.call('ffmpeg -i test_video.mp4 -ss 00:00:07 -to 00:00:13 -async 1 cut.mp4', shell=True)
